1e6bd059500fd9744db754de133ea1f35d65b7fe
vaibhav82000 <vaibhavjaipur82000@gmail.com>
2023-08-14 01:02:53 +0530

    import changes

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,8 +1,8 @@
 import { useTitle } from "../../hooks/useTitle";
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
-import { Testimonials } from "./components/Testimonials";
 import { Faq } from "./components/Faq";
+import { Testimonials } from "./components/Testimonials";
 
 export const HomePage = () => {
   useTitle("Access Latest Computer Science eBooks");
